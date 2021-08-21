7f5fa5e0ee4c8d6661307df5703973d8810a64f0
GregBus <grzegorz.bus@dibby.pl>
2021-08-21 18:51:36 -0400

    fixed repository

--- src/components/Contact-us.js
+++ src/components/Contact-us.js
@@ -2,15 +2,15 @@ import * as React from "react"
 
 const ContactUs = () => (
 <section id="contact">
-	<div class="text-white py-20" style={{"background": "linear-gradient(90deg, #0556a5 0%, #8dc63f 100%)"}}>
-		<div class="container mx-auto flex flex-col md:max-w-6xl md:flex-row my-6 md:my-24">
-			<div class="flex flex-col w-full md:w-1/2 p-8">
+	<div className="text-white py-20" style={{"background": "linear-gradient(90deg, #0556a5 0%, #8dc63f 100%)"}}>
+		<div className="container mx-auto flex flex-col md:max-w-6xl md:flex-row my-6 md:my-24">
+			<div className="flex flex-col w-full md:w-1/2 p-8">
 
-				<p class="ml-6 text-green-300 text-lg uppercase tracking-loose">CONTACT US</p>
+				<p className="ml-6 text-green-300 text-lg uppercase tracking-loose">CONTACT US</p>
 
-				<p class="text-3xl md:text-5xl my-4 leading-relaxed md:leading-snug">CERTIFIED AGENTS ARE STANDING BY</p>
+				<p className="text-3xl md:text-5xl my-4 leading-relaxed md:leading-snug">CERTIFIED AGENTS ARE STANDING BY</p>
 
-				<p class="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
+				<p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
 
 					Gain Access to the best plans in the Health Insurance Market available for you! We will go over every credit, service and program that you or your loved ones qualify for. When it comes to insurance, the plan you choose and the benefits you get, make all the difference. Let our specially trained, experienced and certified agents help you find the right plan for you.
 				</p>
@@ -18,49 +18,49 @@ const ContactUs = () => (
 
 			
 			
-	<div class="flex flex-col w-full lg:w-1/2 justify-center">
-	<div class="container w-full px-4">
-	<div class="flex flex-wrap justify-center">
-	<div class="w-full lg:w-full px-4">
-	<div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-white">
-	<div class="flex-auto p-5 lg:p-10">
+	<div className="flex flex-col w-full lg:w-1/2 justify-center">
+	<div className="container w-full px-4">
+	<div className="flex flex-wrap justify-center">
+	<div className="w-full lg:w-full px-4">
+	<div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-white">
+	<div className="flex-auto p-5 lg:p-10">
 	
 	<form id="appointment" action="#" method="">
 										
-	<div class="grid grid-cols-2 gap-4 max-w-xl m-auto">
+	<div className="grid grid-cols-2 gap-4 max-w-xl m-auto">
  
-    <div class="col-span-2 lg:col-span-1">
-      <input type="text" class="text-gray-500 border-solid border-gray-400 border p-3 md:text-xl w-full" placeholder="Name"/>
+    <div className="col-span-2 lg:col-span-1">
+      <input type="text" className="text-gray-500 border-solid border-gray-400 border p-3 md:text-xl w-full" placeholder="Name"/>
     </div>
 
-    <div class="col-span-2 lg:col-span-1">
-      <input type="tel" class="text-gray-500 border-solid border-gray-400 border p-3 md:text-xl w-full" placeholder="Telephone"/>
+    <div className="col-span-2 lg:col-span-1">
+      <input type="tel" className="text-gray-500 border-solid border-gray-400 border p-3 md:text-xl w-full" placeholder="Telephone"/>
     </div>
 											
-    <div class="col-span-2 lg:col-span-1">
-      <input type="email" class="text-gray-500 border-solid border-gray-400 border p-3 md:text-xl w-full" placeholder="E-Mail"/>
+    <div className="col-span-2 lg:col-span-1">
+      <input type="email" className="text-gray-500 border-solid border-gray-400 border p-3 md:text-xl w-full" placeholder="E-Mail"/>
     </div>
 
-    <div class="col-span-2 lg:col-span-1">
-      <input type="text" class="text-gray-500 border-solid border-gray-400 border p-3 md:text-xl w-full" placeholder="Language"/>
+    <div className="col-span-2 lg:col-span-1">
+      <input type="text" className="text-gray-500 border-solid border-gray-400 border p-3 md:text-xl w-full" placeholder="Language"/>
     </div>
 			
-    <div class="col-span-2 lg:col-span-1">
-      <input type="date" class="text-gray-500 border-solid border-gray-400 border p-3 md:text-xl w-full" placeholder="Date"/>
+    <div className="col-span-2 lg:col-span-1">
+      <input type="date" className="text-gray-500 border-solid border-gray-400 border p-3 md:text-xl w-full" placeholder="Date"/>
     </div>
 
-    <div class="col-span-2 lg:col-span-1">
-      <input type="time" class="text-gray-500 border-solid border-gray-400 border p-3 md:text-xl w-full" placeholder="Time"/>
+    <div className="col-span-2 lg:col-span-1">
+      <input type="time" className="text-gray-500 border-solid border-gray-400 border p-3 md:text-xl w-full" placeholder="Time"/>
     </div>										  
 
 										  
-	<div class="col-span-2">
-      <textarea cols="30" rows="4" class="text-gray-500 border-solid border-gray-400 border p-3 md:text-xl w-full" placeholder="Message"></textarea>
+	<div className="col-span-2">
+      <textarea cols="30" rows="4" className="text-gray-500 border-solid border-gray-400 border p-3 md:text-xl w-full" placeholder="Message" />
     </div>
 
 
-    <div class="col-span-2 text-center">
-<button class="bg-blue-700 hover:bg-blue-900 text-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider">
+    <div className="col-span-2 text-center">
+<button className="bg-blue-700 hover:bg-blue-900 text-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider">
         SUBMIT
       </button>    </div>
 		</div>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,21 +7,21 @@
 
 import * as React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 import Footer from "./Footer"
 
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
+  //
   return (
     <>
       <main>{children}</main>
