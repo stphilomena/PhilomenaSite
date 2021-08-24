bbf9bcc44d4e689d3834bc0b617908bf4615afd5
Dennis <dlindo@gmail.com>
2021-08-23 22:38:35 -0400

    commit

--- src/components/ContactHeader.js
+++ src/components/ContactHeader.js
@@ -3,10 +3,11 @@ import * as React from "react"
 
 const ContactHeader = () => (
 
-    <div className="w-screen h-1/5">
-GOOGLE MAPS
-    </div>
-
+    <div className={"w-full h-120"}>
+            <iframe title={"Our Lady of Victory Catholic Church"}
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3584.064784493577!2d-80.23499528497285!3d26.06413908350025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9a83557faaa31%3A0x6beb6162a59b928!2sOur%20Lady%20of%20Victory%20Catholic%20Church!5e0!3m2!1sen!2sus!4v1629771082042!5m2!1sen!2sus"
+                    width="100%" height="100%" style={{"border": 0}} allowfullscreen="true" loading="lazy"/>
+        </div>
 )
 
 export default ContactHeader
