c182c36ec98ce801b1f08f2b2c8dec682e4e899c
Dennis <dlindo@gmail.com>
2021-08-31 23:27:06 -0400

    commit

--- src/components/CartContent.js
+++ src/components/CartContent.js
@@ -2,7 +2,16 @@ import * as React from "react"
 
 
 const CartContent = () => (
-   <div></div>
+   <div className="p-5  grid grid-cols-3 gap-5 justify-start w-full">
+       <div className="bg-gray-300 cols-span-1 w-full centerAll">
+        IMAGE           
+       </div>
+       <div className="w-2/3 grid grid-rows-3 justify-start">
+       <div>Title</div>
+       <div>Quantity: </div>
+       <div>Total</div>
+       </div>
+   </div>
     )
     
     export default CartContent
