471f5c6ca0370f56f778114d0a68c72ee35496ab
Dennis <dlindo@gmail.com>
2021-09-16 22:27:20 -0400

    commit

--- src/components/CartPageContent.js
+++ src/components/CartPageContent.js
@@ -46,38 +46,38 @@ const CartPageContent = () => {
 <div className="text-red-500 font-black text-4xl centerAll py-10">SHOPPING CART</div>
 <div className="w-full centerAll">
     
-    <div className=" w-10/12">
+    <div className="w-10/12 border-b border-gray-300">
     <div className="hidden md:grid md:grid-cols-12 gap-5 pb-5">
-                            <div className="md:col-span-4 lg:col-span-2 centerAll font-bold">IMAGE</div>
-                            <div className="md:col-span-2 lg:col-span-1 centerAll font-bold">PRICE</div>
-                            <div className="md:col-span-2 lg:col-span-1 centerAll font-bold">QTY</div>
-                            <div className="md:col-span-4 lg:col-span-8 flex justify-start font-bold">PRODUCT DETAILS
-                            </div>
-                        </div>
+            <div className="md:col-span-4 lg:col-span-2 centerAll font-bold">IMAGE</div>
+            <div className="md:col-span-2 lg:col-span-1 centerAll font-bold">PRICE</div>
+            <div className="md:col-span-2 lg:col-span-1 centerAll font-bold">QTY</div>
+            <div className="md:col-span-4 lg:col-span-8 flex justify-start font-bold">PRODUCT DETAILS
+            </div>
+    </div>
 
-                        <div className="p-2">
-                            <div className="grid grid-cols-12 gap-2 md:gap-5">
-                                <div className="col-span-12 md:col-span-4 lg:col-span-2 centerAll">
-                                    IMAGE
-                                </div>
+    <div className="p-2">
+        <div className="grid grid-cols-12 gap-2 md:gap-5">
+            <div className="col-span-12 md:col-span-4 lg:col-span-2 centerAll">
+                IMAGE
+            </div>
 
-                                <div
-                                    className="col-span-2 md:col-span-2 lg:col-span-1 centerAll text-lg font-bold text-gray-600">
-                                    $5.00
-                                </div>
+            <div
+                className="col-span-2 md:col-span-2 lg:col-span-1 centerAll text-lg font-bold text-gray-600">
+                $5.00
+            </div>
 
-                                <div className="col-span-2 md:col-span-2 lg:col-span-1 centerAll">
-                                    <div className="centerAll w-10 h-10 border border-black">1</div>
-                                </div>
+            <div className="col-span-2 md:col-span-2 lg:col-span-1 centerAll">
+                <div className="centerAll w-10 h-10 border border-black">1</div>
+            </div>
 
 
-                                <div className="col-span-6 md:col-span-4 lg:col-span-8">
-                                    <div>Product Title</div>
-                                    <div className="text-gray-500"><i>Product Description</i></div>
+            <div className="col-span-6 md:col-span-4 lg:col-span-8">
+                <div>Product Title</div>
+                <div className="text-gray-500"><i>Product Description</i></div>
 
-                                </div>
-                            </div>
-                        </div>
+            </div>
+        </div>
+    </div>
     </div>
 </div>
 
@@ -100,7 +100,7 @@ const CartPageContent = () => {
             </div>
         </div>
     </div>
-    <div className="py-5">
+    <div className="py-5 ">
     <a href="#checkout">
         <button className="bg-red-500 hover:bg-red-700 text-white p-5 w-full font-bold rounded-full">
                 PLACE ORDER
@@ -112,9 +112,9 @@ const CartPageContent = () => {
     </div>   
 </div>
 
+<div className="py-10"> </div>
 
-
-<div id="checkout" className="text-red-500 font-black text-4xl centerAll py-10">CHECK OUT</div>
+<div id="checkout" className="border-t border-b border-gray-300 text-red-500 font-black text-4xl centerAll py-7">CHECK OUT</div>
 
 <div className="centerAll">
 
@@ -162,9 +162,10 @@ const CartPageContent = () => {
                                 </div> 
                             </div>
 
+
         
         <div>
-        <div className="grid grid-cols-4 w-full gap-2">
+        <div className="grid grid-cols-4 w-full gap-2  pl-2">
                                     <div className="col-span-4 centerAll font-bold py-5">
                                         BILLING INFORMATION
 
@@ -205,38 +206,15 @@ const CartPageContent = () => {
         </div>
     </div>
 </div>
-
-<div className="centerAll">
-<div className="grid grid-cols-1 gap-2 lg:grid-cols-2 w-10/12">
-        <div className="grid grid-cols-1 gap-2 lg:grid-cols-2">
-        <div className="col-span-4 centerAll font-bold py-5">
-        CREDIT CARD INFORMATION
-        </div>
-            <InputField name={"cc"} value={cartState.cc} placeholder={"Credit Card Number"}
-                        setter={setter}
-                        className="col-span-4 inputField" />
-            <InputField name={"exp"} value={cartState.exp} placeholder={"Expiration"}
-                        setter={setter}
-                        className="col-span-2 md:col-span-1 inputField" />
-            <InputField name={"svc"} value={cartState.svc} placeholder={"SVC"}
-                        setter={setter}
-                        className="col-span-2 md:col-span-1 inputField" />
-        </div>
-        <div>
-        <div className="col-span-4 centerAll font-bold py-5">
-         HAVE A PAYPAL ACCOUNT?
-         </div>
-         <div className="centerAll">
-            <button className=" w-72 bg-blue-700 hover:bg-blue-900 text-white p-5 font-bold rounded-full">
-            PAY WITH PAYPAL
+         <div className="centerAll py-10">
+            <button className=" w-72 bg-red-500 hover:bg-red-700 text-white p-5 font-bold rounded-full">
+            COMPLETE ORDER
             </button>
         </div>
-        </div>
-    </div>
-</div>
+       
 
-<div className="py-20">
 
+<div className="py-10">
 </div>
 </form>
     )
