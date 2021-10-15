f30c1576ef1cec8984b78b71fb8bdb840bf13c3d
GregBus <grzegorz.bus@dibby.pl>
2021-10-15 00:51:10 -0400

    fixed building 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import Layout from "../components/Layout"
 import Seo from "../components/Seo"
 
 const NotFoundPage = () => (
-  <Layout>
+  <Layout noCart={true}>
     <Seo title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
