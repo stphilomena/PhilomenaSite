047248081efa3a7896d4815603f72b785af3ae7a
sspxdavie <88943859+sspxdavie@users.noreply.github.com>
2021-10-25 22:50:54 -0400

    Update CartPageContent.js

--- src/components/CartPageContent.js
+++ src/components/CartPageContent.js
@@ -133,7 +133,6 @@ const CartPageContent = () => {
 
 </div>
 </div>
-
 <div className="centerAll">
     <div className="w-10/12 py-5">
     <div className=" w-max">
@@ -142,7 +141,6 @@ const CartPageContent = () => {
                     <div>Subtotal:</div>
                     <div>Shipping:</div>
                     <div>Order Total:</div>
-                    <div>/* TEST */</div>
                 </div>
         <div>
         <div className="col-span-1 text-left font-bold ">
@@ -160,6 +158,43 @@ const CartPageContent = () => {
     </a>
    
     </div>
+    
+        
+        <div id="paypal-button-container"></div>
+        <script src="https://www.paypal.com/sdk/js?&client-id=AWg5MHVS4Y01l66Z1Vc0ZCwjuL9VVuXm550CRVnizukRjuZGYwvFrhcdqoXNoKda3kMKREy14Va_WGRU&merchant-id=<Merchant-ID>&currency=USD"></script>
+        <script>
+        paypal.Buttons({
+
+            // Sets up the transaction when a payment button is clicked
+            createOrder: function(data, actions) {
+            return actions.order.create({
+                purchase_units: [{
+                amount: {
+                    value: '77.44' // Can reference variables or functions. Example: `value: document.getElementById('...').value`
+                }
+                }]
+            });
+            },
+
+            // Finalize the transaction after payer approval
+            onApprove: function(data, actions) {
+            return actions.order.capture().then(function(orderData) {
+                // Successful capture! For dev/demo purposes:
+                    console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+                    var transaction = orderData.purchase_units[0].payments.captures[0];
+                    alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+
+                // When ready to go live, remove the alert and show a success message within this page. For example:
+                // var element = document.getElementById('paypal-button-container');
+                // element.innerHTML = '';
+                // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+                // Or go to another URL:  actions.redirect('thank_you.html');
+            });
+            }
+        }).render('#paypal-button-container');
+
+        </script>
+
     </div>
     </div>   
 </div>
