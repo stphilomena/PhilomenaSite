96591b6dfdbf2ec5de7cad5d5d26eee621d0a4fd
sspxdavie <88943859+sspxdavie@users.noreply.github.com>
2021-10-27 05:42:30 -0400

    Update CartPageContent.js

--- src/components/CartPageContent.js
+++ src/components/CartPageContent.js
@@ -162,7 +162,39 @@ const CartPageContent = () => {
 
     </div>
     </div>   
-    <div id="paypal-button-container" className="w-10/12 border-t border-b border-gray-300 text-red-500 font-black text-4xl centerAll py-7">HOLA - TEST</div>
+    <div id="paypal-button-container" className=""></div>
+    <script>
+      paypal.Buttons({
+
+        // Sets up the transaction when a payment button is clicked
+        createOrder: function(data, actions) {
+          return actions.order.create({
+            purchase_units: [{
+              amount: {
+                value: '77.44' // Can reference variables or functions. Example: `value: document.getElementById('...').value`
+              }
+            }]
+          });
+        },
+
+        // Finalize the transaction after payer approval
+        onApprove: function(data, actions) {
+          return actions.order.capture().then(function(orderData) {
+            // Successful capture! For dev/demo purposes:
+                console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+                var transaction = orderData.purchase_units[0].payments.captures[0];
+                alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+
+            // When ready to go live, remove the alert and show a success message within this page. For example:
+            // var element = document.getElementById('paypal-button-container');
+            // element.innerHTML = '';
+            // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+            // Or go to another URL:  actions.redirect('thank_you.html');
+          });
+        }
+      }).render('#paypal-button-container');
+
+    </script>
 </div>
 
 <div className="py-10"> </div>
