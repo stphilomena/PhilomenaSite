7f7cf5a127d87bccca92f1eb6f3cd162890ec309
sspxdavie <88943859+sspxdavie@users.noreply.github.com>
2021-10-27 04:41:04 -0400

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,6 +12,11 @@ import Footer from "./Footer"
 import SideCart from "./SideCart";
 
 const Layout = ({ children, noCart }) => {
+
+var PAYPAL_SCRIPT = 'https://www.paypal.com/sdk/js?client-id=AVMHbhoq5ugxAWgjRLW7ZhDJEL9jOAGIw5TlvRXBCgHEBUhC2xD96nL6nQd-o4wajuw9AP-lSyXZLbbk';
+  var script = document.createElement('script');
+  script.setAttribute('src', PAYPAL_SCRIPT);
+  document.head.appendChild(script);
   // const data = useStaticQuery(graphql`
   //   query SiteTitleQuery {
   //     site {
