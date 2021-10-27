41a497a4d3964f8fe509650e1d47cfb25f48190e
sspxdavie <88943859+sspxdavie@users.noreply.github.com>
2021-10-27 05:40:07 -0400

    Update CartPageContent.js

--- src/components/CartPageContent.js
+++ src/components/CartPageContent.js
@@ -163,8 +163,7 @@ const CartPageContent = () => {
     </div>
     </div>   
 </div>
-        <!-- Set up a container element for the button -->
-        <div id="paypal-button-container" className="">HOLA - TEST</div>
+<div id="paypal-button-container" className="w-10/12 border-t border-b border-gray-300 text-red-500 font-black text-4xl centerAll py-7">HOLA - TEST</div>
 
 <div className="py-10"> </div>
 <div className="centerAll">
