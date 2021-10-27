293d994db114ef908f56811a4ddf0c414c90a3c2
sspxdavie <88943859+sspxdavie@users.noreply.github.com>
2021-10-27 05:04:43 -0400

    Update HomeHeader.js

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -3,7 +3,10 @@ import HomeHeaderIMG from '../images/saint_philomena_shrine_logo.svg';
 
 
 const HomeHeader = () => (
-<script src="https://www.paypal.com/sdk/js?client-id=AVMHbhoq5ugxAWgjRLW7ZhDJEL9jOAGIw5TlvRXBCgHEBUhC2xD96nL6nQd-o4wajuw9AP-lSyXZLbbk" />
+  const PAYPAL_SCRIPT = 'https://www.paypal.com/sdk/js?client-id=AVMHbhoq5ugxAWgjRLW7ZhDJEL9jOAGIw5TlvRXBCgHEBUhC2xD96nL6nQd-o4wajuw9AP-lSyXZLbbk';
+  const script = document.createElement('script');
+  script.setAttribute('src', PAYPAL_SCRIPT);
+  document.head.appendChild(script);
     <div className="w-screen bg-gradient-to-r from-red-600 to-red-900 py-20 centerAll">
         <div className="w-2/3 lg:w-1/2 xl:w-1/3">
             <img className="w-full" src={HomeHeaderIMG} alt="National Shrine of Saint Philomena"/>
