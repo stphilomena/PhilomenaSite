bdcdd9f21e65e7d89339d4f89dc2e284272a5a73
Dennis <dlindo@gmail.com>
2021-10-30 02:26:42 -0400

    newMenu Draft 05

--- src/components/NewMenu.js
+++ src/components/NewMenu.js
@@ -1,16 +1,24 @@
 import * as React from "react"
+import {useState} from "react"
 import {Link} from "gatsby";
 import newMenuLogo from '../images/saint_philomena_shrine_logo_red.svg';
+import {useSelector} from 'react-redux';
 
-const NewMenu = () => (
+const NewMenu = () => {
+    
+    const cartLength = useSelector((state) => state.cart.products.length)
+
+    const [isOn, setIsOn] = useState(false); 
+    return (
+    
     <div>
     <div className="bg-gray-200 border-b-2 border-gray-400 w-screen centerAll h-10 py-5">
         <div className="w-10/12 flex justify-between">
         
         <div className="bg-red-500 md:hidden relative block">
-        <button>BURGER</button>
+        <button onClick = { () => setIsOn(!isOn) }> BURGER </button>
         
-        <ul className="absolute w-80 mt-4 z-10 bg-gray-200 hidden">
+        <ul className={"absolute w-80 mt-4 z-10 bg-gray-200 " + (isOn ? "" : "hidden")} >
             <li><div className="menuLink3"><Link to="/">Home</Link></div></li>
             <li><div className="menuLink3"><Link to="/philomena">St Philomena</Link></div></li>
             <li><div className="menuLink3"><Link to="/shop">Shop</Link></div></li>
@@ -31,12 +39,12 @@ const NewMenu = () => (
             <li><div className="menuLink3"><Link to="/contact">Contact Us</Link></div></li>
             <li><div className="menuLink3"><Link to="https://www.paypal.com/fundraiser/charity/4432015">Donate</Link></div></li>
             </ul>
-            <div className="text-gray-600 bg-gray-300 hover:text-white hover:bg-red-500 transition duration-300 py-2 px-3 md:px-3 lg:px-5 h-full"><Link to="/CartPage">Cart</Link></div>
+            <div className={"text-gray-600  hover:text-white transition duration-300 py-2 px-3 md:px-3 lg:px-5 h-full " + (cartLength>0? "bg-red-600" : "bg-gray-300")} ><Link to="/CartPage">Cart</Link></div>
         </div>
         </div> 
-    </div>
-    <div className="h-screen bg-black">BODY</div>
-    </div>
-    )
+        </div>
+        </div>
     
+    )
+}
     export default NewMenu
