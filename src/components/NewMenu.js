22a24a4e4e772c20ab9adc261264ef74295aba85
Dennis <dlindo@gmail.com>
2021-10-30 03:24:36 -0400

    newMenu Up Live

--- src/components/NewMenu.js
+++ src/components/NewMenu.js
@@ -3,6 +3,7 @@ import {useState} from "react"
 import {Link} from "gatsby";
 import newMenuLogo from '../images/saint_philomena_shrine_logo_red.svg';
 import {useSelector} from 'react-redux';
+import { BsList, BsCart3 } from "react-icons/bs";
 
 const NewMenu = () => {
     
@@ -15,10 +16,10 @@ const NewMenu = () => {
     <div className="bg-gray-200 border-b-2 border-gray-400 w-screen centerAll h-10 py-5">
         <div className="w-10/12 flex justify-between">
         
-        <div className="bg-red-500 md:hidden relative block">
-        <button onClick = { () => setIsOn(!isOn) }> BURGER </button>
+        <div className="mt-2 md:hidden relative block">
+        <button className="hover:text-red-600" onClick = { () => setIsOn(!isOn) }> <BsList size="1.5em"/> </button>
         
-        <ul className={"absolute w-80 mt-4 z-10 bg-gray-200 " + (isOn ? "" : "hidden")} >
+        <ul className={"absolute w-80 mt-1 z-10 bg-gray-200 " + (isOn ? "" : "hidden")} >
             <li><div className="menuLink3"><Link to="/">Home</Link></div></li>
             <li><div className="menuLink3"><Link to="/philomena">St Philomena</Link></div></li>
             <li><div className="menuLink3"><Link to="/shop">Shop</Link></div></li>
@@ -39,7 +40,7 @@ const NewMenu = () => {
             <li><div className="menuLink3"><Link to="/contact">Contact Us</Link></div></li>
             <li><div className="menuLink3"><Link to="https://www.paypal.com/fundraiser/charity/4432015">Donate</Link></div></li>
             </ul>
-            <div className={"text-gray-600  hover:text-white transition duration-300 py-2 px-3 md:px-3 lg:px-5 h-full " + (cartLength>0? "bg-red-600" : "bg-gray-300")} ><Link to="/CartPage">Cart</Link></div>
+            <div className={"text-gray-600  hover:text-white transition duration-300 py-2 px-3 md:px-3 lg:px-5 h-full " + (cartLength>0? "bg-red-600" : "bg-gray-300")} ><Link to="/CartPage"><BsCart3 size="1.5em"/></Link></div>
         </div>
         </div> 
         </div>
