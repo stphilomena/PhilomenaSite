4e6ff6208c01fbca84431d0a6c6dd31af9cbe4c0
GregBus <grzegorz.bus@dibby.pl>
2021-10-30 00:10:51 -0400

    changed paypal env

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,6 @@ import {Link} from "gatsby";
 
 const Menu = () => (
 <div className="bg-gray-200 border-b-2 border-gray-400">
-<script src="https://www.paypal.com/sdk/js?client-id=ASQwqDlBG6cxCWAynbTjbRevQVD7Ihunm4ivgbOF6TUgGIgjtgrVrAiF7CayL3tdnnw6_F8FcRGwlQgL" />
   <ul className="centerAll text-sm lg:text-2xl px-2">
     <li className="menuLink"><Link to="/philomena">St Philomena</Link></li>
     <li className="menuLink"><Link to="/shop">Shop</Link></li>
