dc07ee1b6b1e4165fd2e8ee5d25b3f3632c4d715
Dennis <dlindo@gmail.com>
2021-10-30 00:43:44 -0400

    newMenu Draft 03

--- src/components/NewMenu.js
+++ src/components/NewMenu.js
@@ -6,26 +6,26 @@ const NewMenu = () => (
     <div className="bg-gray-200 border-b-2 border-gray-400 w-screen centerAll h-10 py-5">
         <div className="w-10/12 flex justify-between">
         <div className="bg-red-400 md:hidden relative block">BURGER
-        <ul className="absolute w-80 mt-2 z-10 bg-green-300 hidden">
-            <li><div>Home</div></li>
-            <li><div>St. Philomena</div></li>
-            <li><div>Shop</div></li>
-            <li><div>About Us</div></li>
-            <li><div>Contact Us</div></li>
-            <li><div>Donate</div></li>
+        <ul className="absolute w-80 mt-4 z-10 bg-green-300 block">
+            <li><div className="menuLink3"><Link to="/">Home</Link></div></li>
+            <li><div className="menuLink3"><Link to="/philomena">St Philomena</Link></div></li>
+            <li><div className="menuLink3"><Link to="/shop">Shop</Link></div></li>
+            <li><div className="menuLink3"><Link to="/about">About Us</Link></div></li>
+            <li><div className="menuLink3"><Link to="/contact">Contact Us</Link></div></li>
+            <li><div className="menuLink3"><Link to="https://www.paypal.com/fundraiser/charity/4432015">Donate</Link></div></li>
             </ul>
         </div>
         <div className="bg-yellow-300">LOGO</div>
         <div className="bg-green-300 flex">
             <ul className="hidden md:flex">
-            <li><div>Home</div></li>
-            <li><div>St. Philomena</div></li>
-            <li><div>Shop</div></li>
-            <li><div>About Us</div></li>
-            <li><div>Contact Us</div></li>
-            <li><div>Donate</div></li>
+            <li><div className="menuLink3 hidden lg:block"><Link to="/">Home</Link></div></li>
+            <li><div className="menuLink3"><Link to="/philomena">St Philomena</Link></div></li>
+            <li><div className="menuLink3"><Link to="/shop">Shop</Link></div></li>
+            <li><div className="menuLink3"><Link to="/about">About Us</Link></div></li>
+            <li><div className="menuLink3"><Link to="/contact">Contact Us</Link></div></li>
+            <li><div className="menuLink3"><Link to="https://www.paypal.com/fundraiser/charity/4432015">Donate</Link></div></li>
             </ul>
-            <div>Cart</div>
+            <div className="menuLink3"><Link to="/CartPage">Cart</Link></div>
         </div>
         </div> 
     </div>
