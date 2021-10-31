c4b54dd2a84e5d6cd8e5b281fac135e0cd46837e
Dennis <dlindo@gmail.com>
2021-10-31 06:08:55 -0400

    myrna's changes

--- src/components/AboutHeader.js
+++ src/components/AboutHeader.js
@@ -5,7 +5,7 @@ import {StaticImage} from "gatsby-plugin-image"
 const AboutHeader = () => (
    
 <div className="centerAll bg-gradient-to-tl from-blue-500 via-blue-300 to-white pt-5 md:pt-10">
-<StaticImage className="w-1/2 md:w-1/5" src="../images/Our_lady_of_victory_crop.png" alt="Our Lady of Victory in Davie Florida" />
+<StaticImage className="w-1/2 md:w-1/5" src="../images/our_lady_of_victory_chapel.png" alt="Our Lady of Victory in Davie Florida" />
     <div className="font-black text-white text-4xl sm:text-5xl lg:text-8xl">
     <div>ABOUT THE SHRINE</div>
   </div>
