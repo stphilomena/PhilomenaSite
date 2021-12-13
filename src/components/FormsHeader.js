164cdf2b7ae2542e1092310d5826684cab5127ee
sspxdavie <d.lindo@gmail.com>
2021-12-13 15:14:16 -0500

    test

--- src/components/FormsHeader.js
+++ src/components/FormsHeader.js
@@ -7,7 +7,7 @@ const FormsHeader = () => (
 <div className="centerAll bg-gradient-to-tl from-blue-500 via-blue-300 to-white pt-5 md:pt-10">
 <StaticImage className="w-1/2 md:w-1/5" src="../images/our_lady_of_victory_chapel.png" alt="Our Lady of Victory in Davie Florida" />
     <div className="font-black text-white text-4xl sm:text-5xl lg:text-8xl">
-    <div>FOUNDATIONAL MASSES,</div>
+    <div>FOUNDATIONAL MASSES:</div>
   </div>
 </div>
 )
