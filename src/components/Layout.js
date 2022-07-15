9895f94a4f09097e9752b2256416bbdbcf7247d7
Dennis <dlindo@gmail.com>
2022-07-15 00:39:22 -0400

    fixed latest philomena version

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,45 +7,75 @@
 
 import * as React from "react"
 import PropTypes from "prop-types"
-// import { useStaticQuery, graphql } from "gatsby"
-import Footer from "./Footer";
+import { useStaticQuery, graphql } from "gatsby"
+import Footer from "./Footer"
 // import SideCart from "./SideCart";
 
 const Layout = ({ children, noCart }) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
-  //
+  const { allContentfulProducts } = useStaticQuery(
+    graphql`
+      query ProductsQuery {
+        contentfulShopProducts {
+          category
+          option1name
+          option1price
+          shippingCharge
+          taxCharge
+          title
+          createdAt
+          contentfulid
+          childContentfulShopProductsDescriptionTextNode {
+            description
+          }
+          option1qty
+          option2name
+          option2price
+          option2qty
+          slug
+          image
+        }
+      }
+    `
+  )
+  debugger
+
   return (
     <>
-        <script async type="text/javascript" src="//static.klaviyo.com/onsite/js/klaviyo.js?company_id=WuiJJG" />
-        <script src="https://www.paypal.com/sdk/js?client-id=ASQwqDlBG6cxCWAynbTjbRevQVD7Ihunm4ivgbOF6TUgGIgjtgrVrAiF7CayL3tdnnw6_F8FcRGwlQgL" />
+      <script
+        async
+        type="text/javascript"
+        src="//static.klaviyo.com/onsite/js/klaviyo.js?company_id=WuiJJG"
+      />
+      <script src="https://www.paypal.com/sdk/js?client-id=ASQwqDlBG6cxCWAynbTjbRevQVD7Ihunm4ivgbOF6TUgGIgjtgrVrAiF7CayL3tdnnw6_F8FcRGwlQgL" />
 
-    <svg aria-hidden="true" style={{"position": "absolute", width: 0, height: 0, overflow: "hidden"}} version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-<defs>
-<symbol id="icon-menu" viewBox="0 0 32 32">
-<path d="M2 6h28v6h-28zM2 14h28v6h-28zM2 22h28v6h-28z" />
-</symbol>
-</defs>
-</svg>
+      <svg
+        aria-hidden="true"
+        style={{
+          position: "absolute",
+          width: 0,
+          height: 0,
+          overflow: "hidden",
+        }}
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+      >
+        <defs>
+          <symbol id="icon-menu" viewBox="0 0 32 32">
+            <path d="M2 6h28v6h-28zM2 14h28v6h-28zM2 22h28v6h-28z" />
+          </symbol>
+        </defs>
+      </svg>
 
-
-
-      <main className={'w-screen overflow-hidden'}>{children}</main>
-        <Footer/>
+      <main className={"w-screen overflow-hidden"}>{children}</main>
+      <Footer />
     </>
   )
-};
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-   noCart: PropTypes.bool
+  noCart: PropTypes.bool,
 }
 
 export default Layout
--- src/templates/product.js
+++ src/templates/product.js
@@ -5,51 +5,38 @@ import Menu from "../components/Menu"
 import ProductDetailHeader from "../components/ProductDetailHeader"
 import ProductDetailContent from "../components/ProductDetailContent"
 import Layout from "../components/Layout"
-import {graphql} from "gatsby"
+import { graphql } from "gatsby"
 
+const productDetail = ({ data, pageContext, uri }) => {
+  console.log("Data", pageContext)
+  const { product } = data
 
-const productDetail = ({data, pageContext, uri}) => 
+  console.log(product)
 
-{
-  console.log('Data', pageContext);
-  const { product } = data;
-
-console.log(product);
-
-return (
-  
-
-  <Layout>
-    
-    <Seo title="Saint Philomena Devotional Items | National Shrine of Saint Philomena" />
-    <ProductDetailHeader/>
-    <Menu/>
-    <ProductDetailContent product={product}/>
- 
-  </Layout>
-)
+  return (
+    <Layout>
+      <Seo title="Saint Philomena Devotional Items | National Shrine of Saint Philomena" />
+      <ProductDetailHeader />
+      <Menu />
+      <ProductDetailContent product={product} />
+    </Layout>
+  )
 }
 
 export const query = graphql`
-query($id: Date) {
-  product: googleSpreadsheetInventoryInventory(itemId: {eq: $id}) {
-    
-                option1Name
-                option1Price
-                option1Quantity
-                option2Name
-                option2Price
-                option2Quantity
-                image {
-                  childImageSharp {
-                    gatsbyImageData(placeholder: BLURRED)
-                  }
-                }
-                title
-                description
+  query($id: Date) {
+    product: googleSpreadsheetInventoryInventory(itemId: { eq: $id }) {
+      option1Name
+      option1Price
+      option1Quantity
+      option2Name
+      option2Price
+      option2Quantity
+      title
+      description
       noShipping
+    }
   }
-}
 `
 
 export default productDetail
