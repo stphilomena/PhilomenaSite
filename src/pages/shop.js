32f977be476c8b4b5de2d2f4966b165bad37980d
Dennis <dlindo@gmail.com>
2022-08-04 02:06:16 -0400

    Stuck getting images from GraphQL

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,14 +1,17 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { Link } from "gatsby"
 import Seo from "../components/Seo"
 import Menu from "../components/Menu"
 import Layout from "../components/Layout"
 import SideCart from "../components/SideCart"
 import HomeHeader from "../components/HomeHeader"
 import Card from "../components/Card"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Shop = ({ data }) => {
-  const { category, image } = data.allContentfulShopProducts.nodes
+  const productsData = data.allContentfulShopProducts.nodes
+
   return (
     <Layout>
       <Seo title="Shop Devotional Items | National Shrine of Saint Philomena" />
@@ -16,26 +19,25 @@ const Shop = ({ data }) => {
       <Menu />
       <div className="centerAll py-10">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-5/6 gap-5 p-5">
-          {data.map((image) => {
-            return (
-              <div key={item.itemId} className="centerAll pt-20">
-                <Card item={item} />
-              </div>
-            )
-          })}
+          {productsData.map((productsData) => (
+            <Link to="/shop/" className="bg-red-700 p-5 text-white text-3xl">
+              <img fluid={productsData.allImageSharp} />
+              {productsData.title}
+            </Link>
+          ))}
         </div>
       </div>
     </Layout>
   )
 }
+
 export default Shop
 
 // graphql query for the product details
 export const query = graphql`
-  query AllProductsQuery {
+  query stPhilomenaShop {
     allContentfulShopProducts {
       nodes {
-        image
         contentfulid
         option1name
         option1price
@@ -49,6 +51,14 @@ export const query = graphql`
         }
         createdAt
         category
+        image
+      }
+    }
+    allImageSharp {
+      nodes {
+        fluid {
+          src
+        }
       }
     }
   }
