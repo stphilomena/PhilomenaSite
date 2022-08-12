198868b1fe20ee0751a73393e1814761ec201d3f
Dennis <dlindo@gmail.com>
2022-08-11 22:41:20 -0400

    working on shop

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -7,36 +7,12 @@ import Layout from "../components/Layout"
 import SideCart from "../components/SideCart"
 import HomeHeader from "../components/HomeHeader"
 import Card from "../components/Card"
-import { StaticImage } from "gatsby-plugin-image"
-
-const Shop = ({ data }) => {
-  const productsData = data.allContentfulShopProducts.nodes
-
-  return (
-    <Layout>
-      <Seo title="Shop Devotional Items | National Shrine of Saint Philomena" />
-      <HomeHeader />
-      <Menu />
-      <div className="centerAll py-10">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-5/6 gap-5 p-5">
-          {productsData.map((productsData) => (
-            <Link to="/shop/" className="bg-red-700 p-5 text-white text-3xl">
-              <img fluid={productsData.allImageSharp} />
-              {productsData.title}
-            </Link>
-          ))}
-        </div>
-      </div>
-    </Layout>
-  )
-}
-
-export default Shop
+import { StaticImage, GatsbyImage, getImage, Img } from "gatsby-plugin-image"
 
 // graphql query for the product details
 export const query = graphql`
   query stPhilomenaShop {
-    allContentfulShopProducts {
+    allContentfulShopProducts(sort: { fields: updatedAt, order: DESC }) {
       nodes {
         contentfulid
         option1name
@@ -49,17 +25,49 @@ export const query = graphql`
         description {
           description
         }
-        createdAt
         category
-        image
+        updatedAt
+      }
+      pageInfo {
+        perPage
       }
     }
-    allImageSharp {
-      nodes {
-        fluid {
-          src
-        }
-      }
+    imageSharp {
+      gatsbyImageData(blurredOptions: { width: 100 }, placeholder: BLURRED)
     }
   }
 `
+
+const Shop = ({ data }) => {
+  const productsData = data.allContentfulShopProducts.nodes
+
+  return (
+    <Layout>
+      <Seo title="Shop Devotional Items | National Shrine of Saint Philomena" />
+      <HomeHeader />
+      <Menu />
+      <div className="centerAll py-10 w-10/12 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+          {productsData.map((productsData) => (
+            <div
+              to="/shop/"
+              className="group flex flex-col items-center text-center gap-3 text-gray-600 uppercase"
+            >
+              <StaticImage
+                alt={productsData.title}
+                src="../images/48-inch-st-philomena-cord.jpg"
+                className="opacity-90 group-hover:opacity-100"
+              />
+              {productsData.title}
+              <button className="bg-red-700 block mx-auto px-4 py-2 rounded-md text-white">
+                ADD TO CART
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default Shop
