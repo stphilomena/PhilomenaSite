7fedaa1a6b5cadb0424f90151e413ac4b4b29ac3
sspxdavie <88943859+sspxdavie@users.noreply.github.com>
2022-12-26 20:00:40 -0500

    fixed layout css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,7 +9,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 // import { useStaticQuery, graphql } from "gatsby"
 import Footer from "./Footer"
-import "./layout.css"
+import "/layout.css"
 // import SideCart from "./SideCart";
 
 const Layout = ({ children, noCart }) => {
